5e4318288effc9c51cbabf94ceef4cdd07304470
Franckdevs <106821220+Franckdevs@users.noreply.github.com>
2025-09-05 10:12:02 +0000

    mise ajour

--- resources/views/compagnie/reservation/index2.blade.php
+++ resources/views/compagnie/reservation/index2.blade.php
@@ -67,7 +67,7 @@
                                         </span>
                                     </td>
                                     <td>
-                                        <a href="{{ route('itineraire.show', $voyage->id) }}" class="btn btn-info btn-sm">
+                                        {{-- <a href="{{ route('itineraire.show', $voyage->id) }}" class="btn btn-info btn-sm">
                                             <i class="fa fa-eye"></i> Voir
                                         </a>
                                         <a href="" class="btn btn-primary btn-sm">
@@ -80,7 +80,7 @@
                                                 onclick="return confirm('Supprimer ce voyage ?')">
                                                 <i class="fa fa-trash"></i> Supprimer
                                             </button>
-                                        </form>
+                                        </form> --}}
                                     </td>
                                 </tr>
                                 @endforeach
